9b0de9d2941700d82f88cf5ca7e467558134769a
leonardo.garcia <leonardo.garcia@targetwork.com.br>
2021-11-23 13:39:00 -0300

    Exemplificação de página de Vantagens

---
+++ FLSoftware.AVila.Front/Pages/Vantagens.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FLSoftware.AVila.Front.Pages
+{
+    public class VantagensModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
